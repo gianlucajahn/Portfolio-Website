2a9b4017da210469397f876aa05d2b2f3d5a4e71
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-04-26 23:35:12 +0200

    Commit of Index Page

---
+++ index-script.js
@@ -0,0 +1,36 @@
+/// <reference path="./typings/globals/jquery/index.d.ts" />
+
+// Fade out page (entire body) when a link is being clicked
+$('a').click(function(e) {
+    e.preventDefault();
+    newLocation = this.href;
+    $('body').fadeOut(1500, newpage);
+    });
+    function newpage() {
+    window.location = newLocation;
+}
+
+// Fade in an entire page upon load
+$(document).ready(function(){
+    $('body').css('display', 'none');
+    $('body').fadeIn(1250);
+}); 
+
+// Reload page on EVERY visit, avoiding faded out pages upon using the "back" arrow function in the client's browser
+function Reload() {
+    try {
+      var headElement = document.getElementsByTagName("head")[0];
+    if (headElement && headElement.innerHTML)
+      headElement.innerHTML += "<meta http-equiv=\"refresh\" content=\"1\">";
+    }
+    catch (e) {}
+}
+
+if ((/iphone|ipod|ipad.*os 5/gi).test(navigator.appVersion)) {
+    window.onpageshow = function(evt) {
+      if (evt.persisted) {
+        document.body.style.display = "none";
+        location.reload();
+        }
+    };
+}
