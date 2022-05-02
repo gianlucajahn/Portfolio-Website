81c9000f5fcc0a7ca5008af5748d4d23b2c78c27
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-05-02 17:46:27 +0200

    Add Files for Sources Subpage, Add fade-in functions in ./sources/script.js

---
+++ sources/script.js
@@ -0,0 +1,41 @@
+/// Referencing jQuery's typing path to use jQuery intelliSense
+/// <reference path="../typings/globals/jquery/index.d.ts" />
+
+// Fade out body when a link is being clicked
+$('a').click(function(e) {
+    e.preventDefault();
+    newLocation = this.href;
+    // Fade Out
+    $('body').fadeOut(1500, newpage);
+    });
+    // Open href attribute of target <a> in same window
+    function newpage() {
+    window.location = newLocation;
+  }
+  
+  // Fade in an entire page upon load
+  $(document).ready(function(){
+      // Change the body's display value to none, then fade it in
+      $('body').css('display', 'none');
+      $('body').fadeIn(1250);
+  }); 
+  
+  // Reload page on EVERY visit, avoiding faded out pages upon using the "back" arrow function in the client's browser
+  function Reload() {
+      try {
+        var headElement = document.getElementsByTagName("head")[0];
+      if (headElement && headElement.innerHTML)
+        headElement.innerHTML += "<meta http-equiv=\"refresh\" content=\"1\">";
+      }
+      catch (e) {}
+  }
+  
+  // Responsiveness Feature for iOS users since the reloading function doesn't work on safari
+  if ((/iphone|ipod|ipad.*os 5/gi).test(navigator.appVersion)) {
+      window.onpageshow = function(evt) {
+        if (evt.persisted) {
+          document.body.style.display = "none";
+          location.reload();
+          }
+      };
+  }
